20dc056c6cfa29da5b832375c610db0048deceb7
Dan Ziv <dan.ziv@kaltura.com>
2018-07-25 15:40:56 +0300

    fix demo

--- demo/app.js
+++ demo/app.js
@@ -1,4 +1,4 @@
-const App = config => {
+const App = function(config) {
   this.context = cast.framework.CastReceiverContext.getInstance();
   this.playerManager = this.context.getPlayerManager();
   this.receiverManager = KalturaPlayer.cast.receiver.getReceiverManager(config);
@@ -6,12 +6,12 @@ const App = config => {
   this.context.setLoggerLevel(cast.framework.LoggerLevel.DEBUG);
 };
 
-App.prototype.start = () => {
+App.prototype.start = function() {
   this.addListeners();
   this.context.start();
 };
 
-App.prototype.addListeners = () => {
+App.prototype.addListeners = function() {
   this.playerManager.setMessageInterceptor(cast.framework.messages.MessageType.LOAD, requestData => {
     return this.receiverManager.loadMedia(requestData);
   });
