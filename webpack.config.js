d911b87773d3082240877479d69ac94ff49be731
Dan Ziv <dan.ziv@kaltura.com>
2018-08-08 12:12:02 +0300

    add css and style loaders

--- webpack.config.js
+++ webpack.config.js
@@ -66,6 +66,17 @@ module.exports = {
             }
           }
         ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          }
+        ]
       }
     ]
   },
