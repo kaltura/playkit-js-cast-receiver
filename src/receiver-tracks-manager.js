5bc15633de1f9d569aac384dafcdfbe39aa68772
yair.ansbacher <yairans@gmail.com>
2020-06-28 13:24:20 +0300

    feat(FEC-10160): filter unsupported formats from the configured tracks passed to the receiver

--- src/receiver-tracks-manager.js
+++ src/receiver-tracks-manager.js
@@ -64,9 +64,12 @@ class ReceiverTracksManager {
 
   _setInitialTextTrack(textLanguage: ?string): void {
     const textTracksManager = this._playerManager.getTextTracksManager();
-    if (textLanguage) {
+    const textTracks = this._player.getTracks(TrackType.TEXT);
+    if (textLanguage && textTracks.some(track => track.language === textLanguage)) {
       this._logger.debug('Set initial text track - setActiveByLanguage', textLanguage);
       textTracksManager.setActiveByLanguage(textLanguage);
+    } else {
+      this._logger.warn(`Text track ${textLanguage} doesn't exist in the supported text tracks`);
     }
   }
 
