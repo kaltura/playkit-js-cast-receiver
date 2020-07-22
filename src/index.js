dabc69ab185fa1e672b4d059e043437a114673c6
Dan Ziv <dan.ziv@kaltura.com>
2020-07-22 13:29:59 +0300

    fixes

--- src/index.js
+++ src/index.js
@@ -4,5 +4,8 @@ import {ReceiverManagerAPI} from './receiver-manager-api';
 declare var __VERSION__: string;
 declare var __NAME__: string;
 
+const VERSION = __VERSION__;
+const NAME = __NAME__;
+
 export {ReceiverManagerAPI as Receiver};
-export {__VERSION__ as VERSION, __NAME__ as NAME};
+export {VERSION, NAME};
