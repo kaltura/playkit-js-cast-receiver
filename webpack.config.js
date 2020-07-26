4bf87d74b176aab8766ed2d8eb6d7b882ebc58b2
Dan Ziv <dan.ziv@kaltura.com>
2020-07-26 11:32:02 +0300

    cr fixes

--- webpack.config.js
+++ webpack.config.js
@@ -13,88 +13,89 @@ const plugins = [
   })
 ];
 
-if (!PROD) {
-  plugins.push(
-    new CopyWebpackPlugin({
-      patterns: [
-        {
-          from: '',
-          to: '.'
-        }
-      ]
-    })
-  );
-}
+module.exports = (env, argv) => {
+  const isProd = argv.mode === 'production';
 
-module.exports = {
-  context: __dirname + '/src',
-  entry: {
-    'playkit-cast-receiver': 'index.js'
-  },
-  output: {
-    path: __dirname + '/dist',
-    filename: '[name].js',
-    library: ['KalturaPlayer', 'cast', 'receiver'],
-    libraryTarget: 'umd',
-    umdNamedDefine: true,
-    devtoolModuleFilenameTemplate: './cast/receiver/[resource-path]'
-  },
-  devtool: 'source-map',
-  plugins: plugins,
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        use: [
+  if (!isProd) {
+    plugins.push(
+      new CopyWebpackPlugin({
+        patterns: [
           {
-            loader: 'babel-loader'
+            from: ' ',
+            to: '.'
           }
-        ],
-        exclude: [/node_modules/]
-      },
-      {
-        test: /\.js$/,
-        exclude: [/node_modules/],
-        enforce: 'pre',
-        use: [
-          {
-            loader: 'eslint-loader',
-            options: {
-              rules: {
-                semi: 0
+        ]
+      })
+    );
+  }
+
+  return {
+    context: __dirname + '/src',
+    entry: {
+      'playkit-cast-receiver': 'index.js'
+    },
+    output: {
+      path: __dirname + '/dist',
+      filename: '[name].js',
+      library: ['KalturaPlayer', 'cast', 'receiver'],
+      libraryTarget: 'umd',
+      umdNamedDefine: true,
+      devtoolModuleFilenameTemplate: './cast/receiver/[resource-path]'
+    },
+    devtool: 'source-map',
+    plugins: plugins,
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          use: [
+            {
+              loader: 'babel-loader'
+            }
+          ],
+          exclude: [/node_modules/]
+        },
+        {
+          test: /\.js$/,
+          exclude: [/node_modules/],
+          enforce: 'pre',
+          use: [
+            {
+              loader: 'eslint-loader',
+              options: {
+                rules: {
+                  semi: 0
+                }
               }
             }
-          }
-        ]
-      },
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: 'style-loader'
-          },
-          {
-            loader: 'css-loader'
-          }
-        ]
+          ]
+        },
+        {
+          test: /\.css$/,
+          use: [
+            {
+              loader: 'style-loader'
+            },
+            {
+              loader: 'css-loader'
+            }
+          ]
+        }
+      ]
+    },
+    devServer: {
+      contentBase: __dirname + '/src'
+    },
+    resolve: {
+      modules: [path.resolve(__dirname, 'src'), 'node_modules']
+    },
+    externals: {
+      'kaltura-player-js': {
+        commonjs: 'kaltura-player-js',
+        commonjs2: 'kaltura-player-js',
+        amd: 'kaltura-player-js',
+        root: ['KalturaPlayer']
       }
-    ]
-  },
-  devServer: {
-    contentBase: __dirname + '/src'
-  },
-  resolve: {
-    modules: [path.resolve(__dirname, 'src'), 'node_modules']
-  },
-  externals: {
-    'kaltura-player-js': {
-      commonjs: 'kaltura-player-js',
-      commonjs2: 'kaltura-player-js',
-      amd: 'kaltura-player-js',
-      root: ['KalturaPlayer']
     }
-  },
-  optimization: {
-    minimize: PROD
-  }
+  };
 };
