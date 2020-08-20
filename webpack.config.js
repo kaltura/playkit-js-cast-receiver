72b2e50e89e045030c5ddfba0f8115f222c3b01f
yair.ansbacher <yairans@gmail.com>
2020-08-20 11:27:03 +0300

    remove umdNamedDefine

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,6 @@ module.exports = {
     path: __dirname + '/dist',
     filename: '[name].js',
     library: ['KalturaPlayer', 'cast', 'receiver'],
-    umdNamedDefine: true,
     devtoolModuleFilenameTemplate: './cast/receiver/[resource-path]'
   },
   devtool: 'source-map',
