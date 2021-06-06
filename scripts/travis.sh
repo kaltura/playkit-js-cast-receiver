2c10918fe454120bbbb77973f49d86e15b49fec5
Yuval Keidar <48407737+Yuvalke@users.noreply.github.com>
2021-06-06 12:11:28 +0300

    build(FEC-10700): Improvement for CI/CD (#50)
    1. add the number of commits from the master
    2. handle two calls after deployment for each repo.
    3. releases from the no-master branch won’t be taken to beta/latest release.
    4. curl request check failure.
    5. don't stop the release after conventional-github-releaser failed

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # https://docs.travis-ci.com/user/customizing-the-build/#Implementing-Complex-Build-Steps
 set -ev
- yarn install
- if [ "${TRAVIS_MODE}" = "lint" ]; then
+yarn install
+if [ "${TRAVIS_MODE}" = "lint" ]; then
   yarn run eslint
 elif [ "${TRAVIS_MODE}" = "flow" ]; then
   yarn run flow
@@ -14,9 +14,11 @@ elif [ "${TRAVIS_MODE}" = "release" ] || [ "${TRAVIS_MODE}" = "releaseCanary" ];
     yarn run release --prerelease canary --skip.commit=true --skip.tag=true
     sha=$(git rev-parse --verify --short HEAD)
     echo "Current sha ${sha}"
+    commitNumberAfterTag=$(git rev-list  `git rev-list --tags --no-walk --max-count=1`..HEAD --count)
+    echo "Number of commit from last tag ${commitNumberAfterTag}"
     currentVersion=$(npx -c 'echo "$npm_package_version"')
     echo "Current version ${currentVersion}"
-    newVersion=$(echo $currentVersion | sed -e "s/canary\.[[:digit:]]/canary.${sha}/g")
+    newVersion=$(echo $currentVersion | sed -e "s/canary\.[[:digit:]]/canary.${commitNumberAfterTag}-${sha}/g")
     echo "New version ${newVersion}"
     sed -iE "s/$currentVersion/$newVersion/g" package.json
     sed -iE "s/$currentVersion/$newVersion/g" CHANGELOG.md
@@ -24,11 +26,14 @@ elif [ "${TRAVIS_MODE}" = "release" ] || [ "${TRAVIS_MODE}" = "releaseCanary" ];
     rm CHANGELOG.mdE
   else
     echo "Run conventional-github-releaser"
-    conventional-github-releaser -p angular -t $GH_TOKEN
+    #ignore error to make sure release won't get stuck
+    conventional-github-releaser -p angular -t $GH_TOKEN || true
   fi
   echo "Building..."
   yarn run build
   echo "Finish building"
+elif [ "${TRAVIS_MODE}" = "deploy" ]; then
+  echo "Deploy..."
 else
 	echo "Unknown travis mode: ${TRAVIS_MODE}" 1>&2
 	exit 1
