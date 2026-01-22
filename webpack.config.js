59ce8bd4559d0a8301b3f759117ad8ec49196459
roeedean <ovp-github-actions@kaltura.com>
2026-01-22 09:29:16 +0200

    feat(FEC-14869): Remove style-loader - No s/css files

--- webpack.config.js
+++ webpack.config.js
@@ -54,9 +54,6 @@ module.exports = {
         test: /\.css$/,
         use: [
           {
-            loader: 'style-loader'
-          },
-          {
             loader: 'css-loader'
           }
         ]
